42f2196ab6208b46e8ec468181b259cd4d884402
just-paja <pavel@proboston.net>
2014-11-14 17:48:02 +0100

    Adminer test base

---
+++ test/basics.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+global.pwf = require('pwf.js');
+
+describe('basic', function() {
+	before(require('./utils/create_env'));
+
+	it('tests only sanity of js code', function() {
+
+	});
+});
---
+++ test/utils/create_env.js
@@ -0,0 +1,53 @@
+var dom = require('jsdom');
+
+module.exports = function(next) {
+	if (typeof pwf == 'undefined') {
+		global.pwf = require('pwf.js');
+	}
+
+	if (typeof pwf.window == 'undefined') {
+		dom.defaultDocumentFeatures = {
+			'FetchExternalResources':['script'],
+			'ProcessExternalResources':['script'],
+			'MutationEvents':'2.0',
+			'QuerySelector':false
+		};
+
+		pwf.document = dom.jsdom('<!doctype html><html><head></head><body></body></html>');
+
+		if (pwf.document.createWindow instanceof Function) {
+			pwf.window = pwf.document.createWindow();
+		} else {
+			pwf.window = pwf.document.parentWindow;
+		}
+
+		global.document = pwf.document;
+		global.window = pwf.window;
+		global.sys = {
+			'debug':{
+				'frontend':true
+			}
+		};
+
+		require('../../lib/include');
+
+		if (typeof pwf.jquery.create == 'function') {
+			pwf.jquery.create(pwf.window);
+		}
+
+
+		pwf.body = pwf.jquery('body');
+
+		delete global.document;
+		delete global.window;
+	}
+
+	return pwf.wait_for('class', [
+		'adminer',
+		'adminer.map',
+		'adminer.list',
+		'adminer.editor',
+		'adminer.destroyer',
+		'adminer.obvious'
+	], next);
+};
